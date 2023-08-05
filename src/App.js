d615148d24d731a8f4f62bc6f56cd41bc07c638c
shekhar <shekharmourya@bitcot.com>
2023-08-05 19:51:56 +0530

    base created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import Banner from './Banner';
-import { Header } from './Header';
-import { Footer } from './Footer'
-import { Card } from './Card';
-import './App.css'
 
-function App() {
-
-  const titles = ["Product", "Price", "Explore","Gallery", "Service", "Contact"]
-  
-
+export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <Banner />
-      <div className='row row-cols-1 row-cols-md-3 mx-5'>
-        {titles.map((item, index) => (
-          <Card key={index} title={item}/>
-        ))}
-      
-      </div>
-      <Footer />
+    <div>
+      <h1>To do list</h1>
     </div>
-  );
+  )
 }
-
-export default App;
