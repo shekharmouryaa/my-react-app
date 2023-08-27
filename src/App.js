65114e281da360f8ef3c659552fa57755fd6ab3f
shekhar <shekharmourya@bitcot.com>
2023-08-27 22:01:39 +0530

    react base with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
-import Banner from './Banner';
-import { Header } from './Header';
-import { Footer } from './Footer'
 import './App.css'
-import { ToastContainer } from 'react-toastify';
 
 function App() {
 
-  const featuresItem = [{
-    title: "Product",
-    description: "Explore our products",
-  }, {
-    title: "Price",
-    description: "Explore our price",
-  }, {
-    title: "Contact",
-    description: "Contact us",
-  }, {
-    title: "About",
-    description: "About us",
-  },
-  {
-    title: "Careers",
-    description: "Join our Team",
-  },
-  {
-    title: "Privacy Policy",
-    description: "Know our Privacy Policy",
-  }
-  ]
-
   return (
     <div className="App">
-      <ToastContainer />
-      <Header />
-      <Banner featuresItem={featuresItem}/>
-      <Footer />
+      <h1>Welcome !</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
 
-  // <React.StrictMode>
-    
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
