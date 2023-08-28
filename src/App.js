fc6838816272d33554a95aefc078c57df04bbf64
shekhar <shekharmourya@bitcot.com>
2023-08-28 07:58:49 +0530

    voice transcript app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css"
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-function App() {
+const App = () => {
+    const [isRecording, setIsRecording] = useState(false)
+    // const [yourName, setYourName] = useState([])
+    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
 
-  return (
-    <div className="App">
-      <h1>Welcome !</h1>
-    </div>
-  );
-}
+    const startListening = () => {
+        SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+        setIsRecording(true)
+    }
+
+    useEffect(() =>{
+    if(transcript.includes("women")){
+        console.log(`Its your turn - ${new Date()}`);
+    }
+
+    },[transcript])
+    
+    const StopRecording=()=>{
+        SpeechRecognition.stopListening()
+        setIsRecording(false)
+    }
+
+    if (!browserSupportsSpeechRecognition) {
+        alert("Please try another browser")
+    }
+
+
+    return (
+        <>
+            <div className="container">
+                <h2>Speech to Text Converter</h2>
+                <br/>
+                <p>A React hook that converts speech from the microphone to text and makes it available to your React
+                    components.</p>
+                <div className="main-content">
+                    {transcript}
+                </div>
+
+                <div className="btn-style">
+
+                  {!isRecording ?  <button className={`btn m-2 btn-light`} onClick={startListening}>Start Listening</button> :
+                   <button className={`btn m-2 btn-dark`} onClick={StopRecording}>Stop Listening</button> }
+
+                </div>
+
+            </div>
+
+        </>
+    );
+};
 
 export default App;
