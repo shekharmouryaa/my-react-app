c9a25493be2d9393e36a23c487e8574d7309dc2f
shekhar <shekharmourya@bitcot.com>
2023-09-14 08:43:30 +0530

    functionality implemented

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -1,21 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PassBook } from './PassBook';
 
 const MainContainer = () => {
   
-  // const [form, setForm] = useState({})
+  const defaultForm = {
+    chequeNumber : "",
+    amount:"",
+    type:"",
+    remainingBalance : 0
+  }
+  const [form, setForm] = useState(defaultForm)
+  const [entries, setEnteries] = useState([])
+  const [balance, setBalance] = useState(0)
 
-  // const handleChange = (e) => {
-  //   setForm({ ...form, [e.target.name]: e.target.value })
-  // }
+  const handleChange = (e) => {
+    if(e.target.name === "amount"){
+      setForm({ ...form, [e.target.name]: Number(e.target.value) })
+    }else{
+      setForm({ ...form, [e.target.name]: e.target.value })
+    }
+  }
 
- 
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    if(form.type ==="credit"){
+      let newBalance = balance + form.amount
+      let updatedForm = {...form, remainingBalance : newBalance}
+      let records = entries.concat(updatedForm)
+      setBalance(newBalance)
+      setEnteries(records)
+    }else{
+      let newBal = balance - form.amount
+      let updatedForm = {...form, remainingBalance : newBal}
+      let records = entries.concat(updatedForm)
+      setBalance(newBal)
+      setEnteries(records)
+    }
+    setForm(defaultForm)
+  }
+
+  console.log(entries)
+
 
   return (
     <div>
       <div class="container wrapper  px-4">
         <div class="row align-items-center py-5">
-          <h1>Hello</h1>          
+          <h1>Entry Form </h1> 
+          <div>
+            <form className="ui form m-2 p-2" onSubmit={(e) => handleSubmit(e)}>
+                <div className='field' ><h3>Add Deposit</h3></div>
+                <div className="field mb-2">
+                    <label>Cheque Number:</label>
+                    <input name='chequeNumber' type='number' value={form.chequeNumber} onChange={e => handleChange(e)} />
+                </div>
+                <div className="field">
+                    <label>Amount:</label>
+                    <input name='amount' type='number' value={form.amount} onChange={e => handleChange(e)} />
+                </div>
+                <div className="field">
+                    <label>Type:</label>
+                    <input name='type' className='m-2' id="credit" type='radio' value={"credit"} checked={form.type ==="credit" ? true : false} onChange={e => handleChange(e)} /> 
+                    <label htmlFor='credit'>Credit</label>
+                    <input name='type' className='m-2' id="debit" type='radio' value={"debit"} checked={form.type ==="debit" ? true : false} onChange={e => handleChange(e)} /> 
+                    <label htmlFor='debit'>Debit</label>
+                    
+                </div>
+                <button type='submit' className='ui button'>Submit</button>
+            </form>
+        </div>         
         </div>
+        <h3>Balance : {balance}</h3>
+        <PassBook entries={entries}/>
       </div>
     </div>
   )
--- src/PassBook.jsx
+++ src/PassBook.jsx
@@ -1,43 +1,34 @@
 import React from 'react'
 
-export const PassBook = ({ records, ConfirmDelete,EditRecords }) => {
+export const PassBook = ({ entries }) => {
     return (
         <div>
             <div className='table-wrapper'>
                 <table class="table">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Mobile</th>
-                            <th scope="col">City</th>
-                            <th scope="col">Occupation</th>
-                            <th scope="col">Delete</th>
-                            <th scope="col">Edit</th>
+                            <th>#</th>
+                            <th>Cheue Number</th>
+                            <th>Type</th>
+                            <th>Amount</th>
+                            <th>Current Balance</th>
+                        
                         </tr>
                     </thead>
                     <tbody>
-                        {records?.map((item, index) => (
+                        {entries?.map((item, index) => (
                             <tr key={index}>
                                 <th scope="row">{index + 1}</th>
-                                <td>{item?.name}</td>
-                                <td>{item?.email}</td>
-                                <td>{item?.phoneNumber}</td>
-                                <td>{item?.city}</td>
-                                <td>{item?.occupation}</td>
-                                <td>
-                                <span className='btn btn-danger pointer-cursor ' onClick={() => ConfirmDelete(item._id)}>{"Delete"}</span>
-                                </td>
-                                <td>
-                                <span className='btn btn-info pointer-cursor mx-2 ' onClick={() => EditRecords(item._id)}>{"Edit"}</span>
-                                </td>
+                                <td>{item?.chequeNumber}</td>
+                                <td>{item?.type}</td>
+                                <td>{item?.amount}</td>
+                                <td>{item?.remainingBalance}</td>
                             </tr>
                         ))
                         }
                     </tbody>
                 </table>
-                {records.length === 0 ? <h4 className='m-3 text-center'>{"No Records found"}</h4> : null}
+                {entries.length === 0 ? <h4 className='m-3 text-center'>{"No Records found"}</h4> : null}
 
             </div>
         </div>
